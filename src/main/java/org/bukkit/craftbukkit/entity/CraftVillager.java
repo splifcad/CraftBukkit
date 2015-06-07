46327ceda77c04d99fec14dec7a2489aa1c81f13
md_5 <git@md-5.net>
2015-06-07 18:23:55 +1000

    [SPIGOT-355] Add Villager career and assorted APIs.

--- src/main/java/org/bukkit/craftbukkit/entity/CraftVillager.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftVillager.java
@@ -10,6 +10,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 public class CraftVillager extends CraftAgeable implements Villager, InventoryHolder {
+
     public CraftVillager(CraftServer server, EntityVillager entity) {
         super(server, entity);
     }
@@ -40,5 +41,130 @@ public class CraftVillager extends CraftAgeable implements Villager, InventoryHo
     @Override
     public Inventory getInventory() {
         return new CraftInventory(getHandle().inventory);
+    }
+
+    @Override
+    public Career getCareer() {
+        switch (getProfession()) {
+            case FARMER:
+                switch (getHandle().bx) {
+                    case 1:
+                        return Career.FARMER;
+                    case 2:
+                        return Career.FISHERMAN;
+                    case 3:
+                        return Career.SHEPHERD;
+                    case 4:
+                        return Career.FLETCHER;
+                }
+                break;
+            case LIBRARIAN:
+                switch (getHandle().bx) {
+                    case 1:
+                        return Career.LIBRARIAN;
+                }
+                break;
+            case PRIEST:
+                switch (getHandle().bx) {
+                    case 1:
+                        return Career.CLERIC;
+                }
+                break;
+            case BLACKSMITH:
+                switch (getHandle().bx) {
+                    case 1:
+                        return Career.ARMOR;
+                    case 2:
+                        return Career.WEAPON;
+                    case 3:
+                        return Career.TOOL;
+                }
+            case BUTCHER:
+                switch (getHandle().bx) {
+                    case 1:
+                        return Career.BUTCHER;
+                    case 2:
+                        return Career.LEATHER;
+                }
+                break;
+        }
+
+        throw new IllegalArgumentException("Unknown career for villager " + this);
+    }
+
+    @Override
+    public void setCareer(Career career) {
+        switch (getProfession()) {
+            case FARMER:
+                switch (career) {
+                    case FARMER:
+                        getHandle().bx = 1;
+                        return;
+                    case FISHERMAN:
+                        getHandle().bx = 2;
+                        return;
+                    case SHEPHERD:
+                        getHandle().bx = 3;
+                        return;
+                    case FLETCHER:
+                        getHandle().bx = 4;
+                        return;
+                }
+            case LIBRARIAN:
+                switch (career) {
+                    case LIBRARIAN:
+                        getHandle().bx = 1;
+                        return;
+                }
+            case PRIEST:
+                switch (career) {
+                    case CLERIC:
+                        getHandle().bx = 1;
+                        return;
+                }
+            case BLACKSMITH:
+                switch (career) {
+                    case ARMOR:
+                        getHandle().bx = 1;
+                        return;
+                    case WEAPON:
+                        getHandle().bx = 2;
+                        return;
+                    case TOOL:
+                        getHandle().bx = 3;
+                        return;
+                }
+            case BUTCHER:
+                switch (career) {
+                    case BUTCHER:
+                        getHandle().bx = 1;
+                        return;
+                    case LEATHER:
+                        getHandle().bx = 2;
+                        return;
+                }
+        }
+
+        throw new IllegalArgumentException("Unknown career " + career + " for villager " + this);
+    }
+
+    @Override
+    public int getCareerLevel() {
+        return getHandle().by;
+    }
+
+    @Override
+    public void setCareerLevel(int level) {
+       getHandle().by = level;
+    }
+
+    @Override
+    public boolean isWilling() {
+        return getHandle().bu;
+    }
+
+    @Override
+    public void setWilling(boolean willing) {
+        getHandle().bu = willing;
     }
 }
